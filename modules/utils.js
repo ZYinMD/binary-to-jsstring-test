0081f2ece8cf3ea7a2eb988889c5ea13b09f59a9
Z Yin <Z.Yin.MD@gmail.com>
2023-09-23 17:32:50 -0400

    add test with base64

--- modules/utils.js
+++ modules/utils.js
@@ -7,5 +7,20 @@ export function randomInteger(ceiling) {
 
 /** create a random length (up to 10M) Uint8Array containing random bytes. */
 export function randomUint8Array() {
-  return new Uint8Array(randomInteger(100)).map(() => randomInteger(256));
+  return new Uint8Array(randomInteger(200)).map(() => randomInteger(256));
+}
+
+/** compare2Uint8Array */
+export function compare2Uint8Array(arr1, arr2) {
+  console.info(`arr1 length ${arr1.length} arr2 length ${arr2.length}`);
+  if (arr1.length !== arr2.length) return false;
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      console.error(
+        `arr1 length ${arr1.length} arr2 length ${arr2.length} first unequal at length ${index}`
+      );
+      return false;
+    }
+  }
+  return true;
 }
